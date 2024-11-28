f10d0a60d8d20834648cb121a2081dcbc0ba9816
roshaan12 <115107064+roshaan12@users.noreply.github.com>
2024-11-28 19:17:42 +0500

    migration 4 files

---
+++ database/migrations/2024_11_28_141441_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id(); // Creates an auto-incrementing primary key
+            $table->string('name'); // Product name
+            $table->text('description'); // Product description
+            $table->decimal('price', 10, 2); // Product price (10 digits total, 2 decimal places)
+            $table->string('image')->nullable(); // Product image (nullable in case there's no image)
+            $table->timestamps(); // created_at and updated_at columns
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2024_11_28_141459_create_product_props_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_props', function (Blueprint $table) {
+            $table->id(); // Auto-incrementing primary key 'id'
+            $table->string('meta_key'); // Meta key (e.g., 'color', 'size')
+            $table->text('meta_value'); // Meta value corresponding to the meta key
+            $table->string('meta_key_type'); // Type of the meta key (e.g., 'string', 'integer', 'boolean')
+            $table->timestamps(); // created_at and updated_at columns
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_props');
+    }
+};
