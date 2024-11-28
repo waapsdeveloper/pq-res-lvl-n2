613a6cdb2c778a2f24c4501861114ae76a2dddd9
waapsdeveloper <shoaibuddin12fx@gmail.com>
2024-11-28 21:13:49 +0500

    .......

--- app/Http/Resources/Admin/OrderResource.php
+++ app/Http/Resources/Admin/OrderResource.php
@@ -7,13 +7,24 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderResource extends JsonResource
 {
-    /**
+     /**
      * Transform the resource into an array.
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
+    public function toArray($request)
     {
-        return parent::toArray($request);
+        $obj = self::toObject($this);
+        return $obj;
+    }
+
+    public static function toObject($obj, $lang = 'en')
+    {
+        return [
+            "id" => $obj->id,
+            "name" => $obj->name,
+            "address" => $obj->address,
+            "status" => $obj->status,
+        ];
     }
 }
