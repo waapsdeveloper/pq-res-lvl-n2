dfc2227dd861f3a86872d9705bcb8da506c1eef6
Waapsdeveloper <99804964+waapsdeveloper@users.noreply.github.com>
2024-11-28 19:53:36 +0500

    categories table

--- database/migrations/2024_11_28_124511_create_categories_table.php
+++ database/migrations/2024_11_28_124511_create_categories_table.php
@@ -12,12 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-        $table->id(); // Creates an auto-incrementing unsigned big integer as 'id'
-        $table->string('name'); // Category name
-        $table->string('slug')->unique(); // Unique slug
-        $table->foreignId('category_id')->nullable()->constrained('categories')->onDelete('cascade'); // Parent category ID
-        $table->timestamps(); // created_at and updated_at columns
-    });
+            $table->id(); // Creates an auto-incrementing unsigned big integer as 'id'
+            $table->string('name'); // Category name
+            $table->string('slug')->unique(); // Unique slug
+            $table->foreignId('category_id')->nullable()->constrained('categories')->onDelete('cascade'); // Parent category ID
+            $table->timestamps(); // created_at and updated_at columns
+        });
     }
 
     /**
