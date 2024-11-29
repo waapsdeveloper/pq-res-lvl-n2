80c92b5030ddbfa238d4dd443b86d0175b13bb65
Waapsdeveloper <99804964+waapsdeveloper@users.noreply.github.com>
2024-11-29 14:24:04 +0500

    products model update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $table = 'products';
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'price',
+        'image'
+    ];
 }
