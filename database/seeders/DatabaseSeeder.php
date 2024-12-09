4d17b2ac2d008090754104fe0b41500b687ea0c9
AHSAN AHMED <97437782+MRAHSANAHMED@users.noreply.github.com>
2024-12-09 15:46:51 +0500

    some changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,7 +29,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(OrderSeeder::class);
         $this->call(OrderProductSeeder::class);
-        $this->call(SessionsSeeder::class);
 
 
     }
