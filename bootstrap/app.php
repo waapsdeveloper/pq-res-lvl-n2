93eee245fa1a18b4e43716211eed97665cea1a81
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2024-12-10 22:57:54 +0500

    ss

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'admin/*', // Excludes all routes starting with 'admin/'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
