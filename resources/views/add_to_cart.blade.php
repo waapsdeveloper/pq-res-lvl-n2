5d4ef62ef955c762f3640536670d0df31a1a9559
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2024-12-10 15:19:05 +0500

    add to cart

---
+++ resources/views/add_to_cart.blade.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta content="width=device-width, initial-scale=1.0" name="viewport">
+      <title>The Local</title>
+      <meta name="description" content="">
+      <meta name="keywords" content="">
+      <!-- Favicons -->
+      <link href="assets/img/favicon.png" rel="icon">
+      <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+      <!-- Fonts -->
+      <link href="https://fonts.googleapis.com" rel="preconnect">
+      <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Amatic+SC:wght@400;700&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+      <!-- Vendor CSS Files -->
+      <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+      <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+      <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+      <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+      <!-- Main CSS File -->
+      <link href="assets/css/main.css" rel="stylesheet">
+   </head>
+   <body class="index-page">
+      <header id="header" class="header d-flex align-items-center sticky-top">
+         <div class="container position-relative d-flex align-items-center justify-content-between">
+            <a href="index.html" class="logo d-flex align-items-center me-auto me-xl-0">
+               <!-- Uncomment the line below if you also wish to use an image logo -->
+               <img src="assets/img/logo.svg" alt="">
+               <span>.</span>
+            </a>
+            <nav id="navmenu" class="navmenu">
+               <ul>
+                  <li><a href="#hero" class="active">Home<br></a></li>
+                  <li><a href="#about">today deals</a></li>
+                  <li><a href="#menu">About</a></li>
+                  <li><a href="#events">Menu</a></li>
+                  <li><a href="#chefs">Table Booking</a></li>
+                  <li><a href="#gallery">Contact</a></li>
+               </ul>
+               <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+            </nav>
+            <a class="cart-bx" href="" data-aos="fade" data-aos-duration="1000">
+            <span class="cart-count">2</span>
+            <img src="assets/img/cart.png" alt="">
+            </a>
+         </div>
+      </header>
+      <main class="main">
+         <section class="sc-1 inner">
+            <div class="fl-1" >
+               <img src="assets/img/18.png" alt="" data-aos="fade-down" data-aos-duration="1000">
+            </div>
+            <div class="fl-2 animated-2" data-aos="zoom-in" data-aos-duration="1500">
+               <img src="assets/img/7.svg" alt="">
+            </div>
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-12">
+                     <div class="inner-content">
+                        <h1>Add to cart</h1>
+                        <p>HOME / ADD TO CART</p>
+                     </div>
+                  </div>
+
+               </div>
+            </div>
+
+
+            <div class="fl-5 animated-2" data-aos="zoom-in-up" data-aos-duration="1200">
+               <img src="assets/img/2.png" alt="">
+            </div>
+            <div class="fl-6" data-aos="zoom-in-up" data-aos-duration="1300">
+               <img src="assets/img/9.png" alt="">
+            </div>
+            <div class="fl-26" data-aos="zoom-in-up" data-aos-duration="1300">
+                <img src="assets/img/fl-26.svg" alt="">
+             </div>
+         </section>
+
+         <section class="sec-3">
+            <div class="fl-15" data-aos="fade" data-aos-duration="1200">
+               <img src="assets/img/fl-15.svg" alt="">
+            </div>
+            <div class="fl-16" data-aos="fade" data-aos-duration="1200">
+               <img src="assets/img/fl-16.svg" alt="">
+            </div>
+            <div class="fl-17" data-aos="fade" data-aos-duration="1200">
+               <img src="assets/img/fl-17.svg" alt="">
+            </div>
+            <div class="container">
+
+                <div class="add-to-sc">
+                    <div class="row"></div>
+                </div>
+
+            </div>
+         </section>
+
+
+
+      </main>
+      <footer id="footer" class="footer dark-background">
+         <div class="container">
+            <div class="ftr-top">
+               <div class="row">
+                  <div class="col-md-3">
+                     <img src="assets/img/ftr-logo.svg" alt="">
+                  </div>
+                  <div class="col-md-6">
+                     <h3 class="ft-cont">Feel Hunger! Order Your <span>Like Food.</span></h3>
+                  </div>
+                  <div class="col-md-3">
+                     <div class="btn-sc text-center">
+                        <a href="#">
+                        <button class="btn-prime">
+                        Order Now
+                        </button>
+                        </a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="row gy-3">
+               <div class="col-lg-3 col-md-6 d-flex">
+                  <div class="address">
+                     <h4>Address</h4>
+                     <p class="mb-0">570 8th Ave,New York, <br> NY 10018 United States</p>
+                     <a href="#">view Google map</a>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-md-6 d-flex">
+                  <div>
+                     <h4>Book a table</h4>
+                     <p class="mb-0">Dogfood och Sliders foodtruck. <br> Under Om oss kan ni läsa</p>
+                     <a href="#">Make A Call</a>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-md-6 d-flex">
+                  <div>
+                     <h4>Opening Hours</h4>
+                     <p class="mb-0">
+                        <span>monday-friday</span> <span>8AM - 4PM</span><br>
+                        <span>saturday</span> <span>9AM - 5PM</span>
+                     </p>
+                     <a href="#">Make A Call</a>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-md-6">
+                  <h4>Newsletter</h4>
+                  <!-- <div class="social-links d-flex">
+                     <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
+                     <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                     <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                     <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                     </div> -->
+                  <div class="ftr-news">
+                     <input type="email" name="" id="">
+                     <button href="#" class="btn-news">
+                     <img src="assets/img/sent.svg" alt="">
+                     </button>
+                  </div>
+                  <a href="#">Make A Call</a>
+               </div>
+            </div>
+         </div>
+         <div class="container copyright text-center mt-4">
+            <p>© <span>2024</span> <span>The Local Craft Food</span></p>
+         </div>
+      </footer>
+      <!-- Scroll Top -->
+      <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+      <!-- Preloader -->
+      <div id="preloader"></div>
+      <!-- Vendor JS Files -->
+      <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <script src="assets/vendor/php-email-form/validate.js"></script>
+      <script src="assets/vendor/aos/aos.js"></script>
+      <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+      <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+      <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+      <!-- Main JS File -->
+      <script src="assets/js/main.js"></script>
+   </body>
+</html>
