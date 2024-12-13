8df23a4ebfcffb9f5fbd158262b4b5e08e893c6d
AHSAN AHMED <97437782+MRAHSANAHMED@users.noreply.github.com>
2024-12-13 17:04:14 +0500

    view items and code removes

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,3 @@
 use App\Http\Controllers\Frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::permanentRedirect('/', '/');
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/today-deals', [HomeController::class, 'todayDeals'])->name('todayDeals');
-Route::get('/about', [HomeController::class, 'about'])->name('about');
-Route::get('/menu', [HomeController::class, 'menu'])->name('menu');
-Route::get('/table-booking', [HomeController::class, 'tableBooking'])->name('tableBooking');
-Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
-Route::get('/addtocart', [HomeController::class, 'addToCart'])->name('addToCart');
