fe80063e2b1b901a8261522c79f22d5599fa6c9a
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2024-12-14 12:22:40 +0500

    CORS

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Cookie');
-
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
