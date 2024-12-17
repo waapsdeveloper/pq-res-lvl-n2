bd31e896a652ab44c92b4ffc87163a42e4adb926
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2024-12-17 12:23:45 +0500

    check-table-availability

--- routes/frontend.php
+++ routes/frontend.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('table-booking')->group(function () {
     Route::resource('/', TableBookingController::class)
         ->parameters(['' => 'id'])
-        ->only(['index', 'show', 'store', 'update', 'destroy'])
+        ->only(['index', 'show', 'store', 'update', 'destroy', 'check-table-availability'])
         ->names('table-booking');
-    Route::post('/check-table-availability', [TableBookingController::class. 'checkTableAvailability']);
+
 });
 
 
