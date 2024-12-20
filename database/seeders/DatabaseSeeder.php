3455cf6e3e17388b4d44feaa8b66093ba81247e3
AHSAN AHMED <97437782+MRAHSANAHMED@users.noreply.github.com>
2024-12-20 15:34:51 +0500

    seeding changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,19 +15,19 @@ class DatabaseSeeder extends Seeder
         // Use statements for seeders
         $this->call(RolesTableSeeder::class); //1
         $this->call(UsersTableSeeder::class); //2
-        $this->call(CategorySeeder::class); //3
         $this->call(RestaurantsTableSeeder::class); //4
-        $this->call(OrderSeeder::class); //5
-        $this->call(OrderBillingSeeder::class); //6
-        $this->call(OrderProductSeeder::class); //7
-        $this->call(PaymentSeeder::class); //8
+        $this->call(CategorySeeder::class); //3
         $this->call(ProductSeeder::class); //9
         $this->call(ProductsPropsSeeder::class); //10
-        $this->call(ProfileSeeder::class); //11
+        // $this->call(OrderSeeder::class); //5
+        // $this->call(OrderBillingSeeder::class); //6
+        // $this->call(OrderProductSeeder::class); //7
+        // $this->call(PaymentSeeder::class); //8
+        // $this->call(ProfileSeeder::class); //11
         $this->call(RTableSeeder::class); //12
-        $this->call(SessionSeeder::class); //13
-        $this->call(UserAddressesSeeder::class); //14
-        $this->call(RTableReservingSeeder::class);
+        // $this->call(SessionSeeder::class); //13
+        // $this->call(UserAddressesSeeder::class); //14
+        // $this->call(RTableReservingSeeder::class);
         $this->call(RestaurantTimingSeeder::class);
     }
 }
