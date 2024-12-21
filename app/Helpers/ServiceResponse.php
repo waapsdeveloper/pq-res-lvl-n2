e6dff95a1b29c9a54cf01339df729f79ca64748c
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2024-12-21 13:55:12 +0500

    error code 400

--- app/Helpers/ServiceResponse.php
+++ app/Helpers/ServiceResponse.php
@@ -5,21 +5,23 @@ namespace App\Helpers;
 class ServiceResponse
 {
 
-    static public function success($msg, $data = [])
+    static public function success($msg, $data = [], $code = 200)
     {
         return [
             'bool' => true,
             'result' => $data,
             'message' => $msg,
+            'code' => $code
         ];
     }
 
-    static public function error($msg, $data = [])
+    static public function error($msg, $data = [], $code = 400)
     {
         return [
             'bool' => false,
             'message' => $msg,
-            'result' => $data
+            'result' => $data,
+            'code' => $code
         ];
     }
 }
