22822f89e6276e412fd0f51d96933a2f2316e11a
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2024-12-21 14:41:41 +0500

    no_of_seats rtable

--- app/Http/Controllers/Admin/RtableController.php
+++ app/Http/Controllers/Admin/RtableController.php
@@ -108,7 +108,7 @@ class RtableController extends Controller
         // Fetch and group the data as before
         $restaurants = Rtable::with('restaurant:id,name')
             ->where('restaurant_id', $id)
-            ->select('id','restaurant_id', 'floor', 'identifier')
+            ->select('id','restaurant_id', 'floor', 'identifier','no_of_seats')
             ->get()
             ->groupBy('restaurant_id');
 
