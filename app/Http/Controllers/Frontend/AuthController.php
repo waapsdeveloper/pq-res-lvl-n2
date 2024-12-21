e2ba73900bfde27c5957b151944fb7b51c0afee7
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2024-12-21 10:04:28 +0500

    auth login register

---
+++ app/Http/Controllers/Frontend/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    /**
+     * Handle user registration.
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return self::success('register successful', ['user' => $user, 'token' => $token]);
+    }
+
+    /**
+     * Handle user login.
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->accessToken;
+
+        return self::success('Login successful', ['user' => $user, 'token' => $token]);
+    }
+
+    /**
+     * Get the authenticated user.
+     */
+    public function me(Request $request)
+    {
+        $user = Auth::user();
+        return self::success('Login successful', ['user' => $user]);
+    }
+
+    /**
+     * Handle user logout.
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
+}
--- routes/frontend.php
+++ routes/frontend.php
@@ -2,8 +2,22 @@
 
 use App\Http\Controllers\Admin\RtableController;
 use App\Http\Controllers\Frontend\RTablesController;
+use App\Http\Controllers\Frontend\AuthController;
 use App\Http\Controllers\Frontend\TableBookingController;
 use Illuminate\Support\Facades\Route;
+
+
+
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::get('/auth/me', [AuthController::class, 'me'])->middleware('auth:api');
+Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+
+
+Route::resource('/rtables', RTablesController::class)
+    ->parameters(['' => 'id'])
+    ->only(['index', 'show', 'store', 'update', 'destroy'])
+    ->names('rtables');
 
 
 Route::prefix('table-booking')->group(function () {
@@ -16,6 +30,7 @@ Route::prefix('table-booking')->group(function () {
         ->only(['index', 'show', 'store', 'update', 'destroy'])
         ->names('table-booking');
 });
+
 Route::resource('/rtables', RTablesController::class)
     ->parameters(['' => 'id'])
     ->only(['index', 'show', 'store', 'update', 'destroy'])
