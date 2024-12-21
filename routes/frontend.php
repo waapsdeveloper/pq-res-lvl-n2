e3cd75908a021b2300d22c27f33cfb2e85736aa0
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2024-12-21 16:54:05 +0500

    updates

--- routes/frontend.php
+++ routes/frontend.php
@@ -7,11 +7,18 @@ use App\Http\Controllers\Frontend\TableBookingController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('login', [AuthController::class, 'login']);
-Route::middleware('auth:api')->group(function () {
-    Route::get('user', [AuthController::class, 'user']);
-    Route::post('logout', [AuthController::class, 'logout']);
-});
+
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::get('/auth/me', [AuthController::class, 'me'])->middleware('auth:api');
+Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+
+
+Route::resource('/rtables', RTablesController::class)
+    ->parameters(['' => 'id'])
+    ->only(['index', 'show', 'store', 'update', 'destroy'])
+    ->names('rtables');
+
 
 Route::prefix('table-booking')->group(function () {
     // Route::get('/getRestaurantsTables', [TableBookingController::class, 'getRestaurantsTables'])->name('getRestaurantsTables');
