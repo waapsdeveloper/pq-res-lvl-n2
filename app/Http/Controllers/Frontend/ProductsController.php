8b5e3a01d344fb94a2dcde741529f2a1992bf30d
AHSAN AHMED <97437782+MRAHSANAHMED@users.noreply.github.com>
2024-12-21 20:50:22 +0500

    changes

--- app/Http/Controllers/Frontend/ProductsController.php
+++ app/Http/Controllers/Frontend/ProductsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Helpers\ServiceResponse;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Admin\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -12,6 +13,10 @@ class ProductsController extends Controller
     public function popdishes()
     {
         $products = Product::limit(8)->get();
+
+        $products->getCollection()->transform(function ($item) {
+            return new ProductResource($item);
+        });
         return ServiceResponse::success('Popular dishes available', ['products' => $products]);
     }
 }
