c0f963be718f4abcf03687579f26c317e1a25e17
AHSAN AHMED <97437782+MRAHSANAHMED@users.noreply.github.com>
2024-12-29 20:07:16 +0500

    push

--- database/migrations/2024_12_09_090609_create_orders_table.php
+++ database/migrations/2024_12_09_090609_create_orders_table.php
@@ -23,7 +23,6 @@ return new class extends Migration {
             $table->string('table_no')->nullable();
             $table->string('total_price')->nullable();
             $table->string('discount')->nullable();
-            $table->string('total_price')->nullable();
             $table->string('restaurant_id')->nullable();
             $table->string('discount')->nullable();
 
