201896cf3daf25cb8b36c5699c2a957902afb12c
AHSAN AHMED <97437782+MRAHSANAHMED@users.noreply.github.com>
2025-01-15 20:02:57 +0500

    name null vari

--- database/migrations/2025_01_08_133823_create_variations_table.php
+++ database/migrations/2025_01_08_133823_create_variations_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('variations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('description')->nullable();
             $table->json('meta_value')->nullable();
             $table->timestamps();
