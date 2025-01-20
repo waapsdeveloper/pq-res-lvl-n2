d71692c256d868a0f63030310e02edf6421bc220
AHSAN AHMED <97437782+MRAHSANAHMED@users.noreply.github.com>
2025-01-20 20:40:06 +0500

    restaurant_settings

--- app/Models/RestaurantSetting.php
+++ app/Models/RestaurantSetting.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class RestaurantSetting extends Model
 {
-    //
+    protected $table = 'restaurant_settings';
+    protected $fillable = [
+        'restaurant_id',
+        'meta_key',
+        'meta_value',
+    ];
+    protected $casts = [
+        'meta_value',
+    ];
 }
