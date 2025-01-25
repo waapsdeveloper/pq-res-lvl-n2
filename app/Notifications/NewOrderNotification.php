353a065ecbc9b7b24ee420e6ab52e47a989299e1
AHSAN AHMED <97437782+MRAHSANAHMED@users.noreply.github.com>
2025-01-25 16:23:51 +0500

    dc

--- app/Notifications/NewOrderNotification.php
+++ app/Notifications/NewOrderNotification.php
@@ -44,11 +44,12 @@ class NewOrderNotification extends Notification
     public function toArray(object $notifiable): array
     {
         // $order = Order::find($this->order->id);
+        // dd($this->order);
         return [
             'message' => 'New order received! ' . $this->order->order_number,
             'order_id' => $this->order->id,
             'order_number' => $this->order->order_number,
-            'customer_name' => $this->order->customer->name,
+            // 'customer_name' => $this->order->customer->name,
             'total_price' => $this->order->total_price,
             'url' => url("/admin/order/{$this->order->id}")
         ];
