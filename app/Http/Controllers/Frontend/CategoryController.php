23e7b1e7219d4c8d34010592d4b167e69a66e6a0
mrahsanahmed <mr.ahsanahmed5@gmail.com>
2025-01-29 20:48:36 +0500

    Update categories method in CategoryController to include full image URL in response

--- app/Http/Controllers/Frontend/CategoryController.php
+++ app/Http/Controllers/Frontend/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Helpers\Helper;
 use App\Helpers\ServiceResponse;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
@@ -12,6 +13,7 @@ class CategoryController extends Controller
     public function categories()
     {
         $categories = Category::get();
+        $categories['image'] = Helper::returnFullImageUrl($categories['image']);
         return ServiceResponse::success('categories are retrived successfully', ['data' => $categories]);
     }
 }
