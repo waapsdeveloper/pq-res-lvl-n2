078901e99771117a693877f4cf63ff115cad6832
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-02-07 20:39:00 +0500

    update

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,7 +8,9 @@ class Customer extends Model
 {
     protected $table = 'users';
 
+    protected $fillable = ['name', 'email', 'phone', 'password', 'status', 'role_id', 'restaurant_id', 'address', 'city', 'state', 'country', 'image'];
 
+    protected $hidden = ['password', 'remember_token'];
 
     // public function addresses()
     // {
