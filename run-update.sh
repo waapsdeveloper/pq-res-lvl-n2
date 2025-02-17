24ab52219202c59e4b55a70d2f7f2f04cd98375e
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-02-17 11:47:10 +0500

    script update

--- run-update.sh
+++ run-update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-php artisan migrate --path="database\migrations\2025_02_17_064006_add_order_number_to_rtable_bookings.php"
+php artisan migrate --path="database/migrations/2025_02_17_064006_add_order_number_to_rtable_bookings.php"
