452d5620ba05e640e64773af90943e6adaa20740
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-03-08 14:01:47 +0500

    migration update

--- run-update.sh
+++ run-update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-php artisan migrate --path="database/migrations/2025_03_07_103629_create_countries_table.php"
-php artisan db:seed --class=CountrySeeder
+#php artisan migrate --path="database/migrations/2025_03_07_103629_create_countries_table.php"
+#php artisan db:seed --class=CountrySeeder
+
+php artisan migrate --path="database/migrations/2025_03_08_085538_add_dial_code_and_phone_to_orders_table.php"
 
