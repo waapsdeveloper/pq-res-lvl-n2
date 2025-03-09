bffa2fbf66a6499c5afab6f26cf80e5985027f7c
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-03-09 22:31:34 +0500

    faf

--- run-update.sh
+++ run-update.sh
@@ -5,6 +5,8 @@
 
 #php artisan migrate --path="database/migrations/2025_03_08_085538_add_dial_code_and_phone_to_orders_table.php"
 
-php artisan make:migration create_coupons_table
+#php artisan make:migration create_coupons_table
+
+
 
 
