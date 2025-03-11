c2d675076e2aacf343fcf86e7477d9db2fe14b44
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-03-11 15:22:39 +0500

    updates

--- app/Http/Resources/Frontend/OrderResource.php
+++ app/Http/Resources/Frontend/OrderResource.php
@@ -32,6 +32,9 @@ class OrderResource extends JsonResource
             'delivery_address' => $obj->delivery_address ?? null,
             'phone' => $obj->phone ? $obj->phone : '',
             'dial_code' => $obj->dial_code ? $obj->dial_code : '',
+            'coupon_code' => $obj->coupon_code,
+            'discount_value' => $obj->discount_value,
+            'final_total' => $obj->final_total,
             'products' => $obj->orderProducts ? $obj->orderProducts->map(function ($orderProduct) {
                 $image = $orderProduct->product ? Helper::returnFullImageUrl($orderProduct->product->image) : null;
                 return [
