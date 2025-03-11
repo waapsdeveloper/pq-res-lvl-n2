0c0310b1957f2a3e5701f626548572fc51961e02
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-03-11 16:33:03 +0500

    update to live 11 03 2025

--- run-update.sh
+++ run-update.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-#php artisan migrate --path="database/migrations/2025_03_07_103629_create_countries_table.php"
-#php artisan db:seed --class=CountrySeeder
-
-#php artisan migrate --path="database/migrations/2025_03_08_085538_add_dial_code_and_phone_to_orders_table.php"
-
-#php artisan migrate --path="database/migrations/2025_03_09_121254_create_coupons_table.php"
-
+php artisan migrate --path="database/migrations/2025_03_07_103629_create_countries_table.php"
+php artisan db:seed --class=CountrySeeder
+php artisan migrate --path="database/migrations/2025_03_08_085538_add_dial_code_and_phone_to_orders_table.php"
+php artisan migrate --path="database/migrations/2025_03_09_121254_create_coupons_table.php"
 php artisan migrate --path="database/migrations/2025_03_10_080113_add_coupon_fields_to_orders_table.php"
 
 
