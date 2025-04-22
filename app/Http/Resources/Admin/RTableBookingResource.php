3ddafe8e9fa8a28e75105a163749d6e8deff9486
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-04-22 23:08:20 +0500

    table booking

--- app/Http/Resources/Admin/RTableBookingResource.php
+++ app/Http/Resources/Admin/RTableBookingResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Admin;
 
+use App\Models\RTablesBooking;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class RTableBookingResource extends JsonResource
@@ -19,8 +20,27 @@ class RTableBookingResource extends JsonResource
 
     public static function toObject($obj, $lang = 'en')
     {
+        $status = $obj->status;
+        $existingBooking = RTablesBooking::where('rtable_id', $obj->rtable_id)
+                ->where(function ($query)  {
+                    $query->whereBetween('booking_start', [now(), now()->addHour()])
+                        ->orWhereBetween('booking_end', [now(), now()->addHour()])
+                        ->orWhere(function ($q) {
+                            $q->where('booking_start', '<=', now())
+                                ->where('booking_end', '>=', now()->addHour());
+                        });
+                })
+                ->first();
+
+            if ($existingBooking) {
+                $status = 'reserved';
+            } else {
+                $status = 'active';                
+            }
+
         return [
             "id" => $obj->id,
+            "rtable_id" => $obj->rtable_id,
             "customer" => $obj->customer,
             "role" => $obj->customer && $obj->customer->role ? $obj->customer->role->name : null,
             "order_id" => $obj->order_id,
@@ -28,7 +48,7 @@ class RTableBookingResource extends JsonResource
             "booking_end" => $obj->booking_end,
             "no_of_seats" => $obj->no_of_seats,
             "description" => $obj->description,
-            "status" => ucfirst($obj->status),
+            "status" => ucfirst($status),
         ];
     }
 }
