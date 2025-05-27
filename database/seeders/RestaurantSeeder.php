8fd188586fa05a582225a4c83a23c75bd86f2776
Muhammad Shayan Bhatti <125445144+ShayanBhatti@users.noreply.github.com>
2025-05-27 11:32:10 -0400

    s

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -2,11 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Helpers\Helper;
+use App\Models\Restaurant;
+use App\Models\BranchConfig;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class RestaurantSeeder extends Seeder
 {
@@ -19,25 +18,32 @@ class RestaurantSeeder extends Seeder
         $restaurants = json_decode(File::get($jsonFilePath), true);
 
         // Insert restaurants into the database
-        foreach ($restaurants as $restaurant) {
-            DB::table('restaurants')->insert([
-                'name' => $restaurant['name'],
-                'address' => $restaurant['address'],
-                'phone' => $restaurant['phone'],
-                'email' => $restaurant['email'],
-                'website' => $restaurant['website'],
-                'description' => $restaurant['description'],
-                'rating' => $restaurant['rating'],
-                'image' => $restaurant['image'],
-                'favicon' => $restaurant['favicon'],
-                'logo' => $restaurant['logo'],
-                'status' => $restaurant['status'],
-                'is_active' => $restaurant['is_active'],
+        foreach ($restaurants as $data) {
+            $restaurant = Restaurant::create([
+                'name' => $data['name'],
+                'address' => $data['address'],
+                'phone' => $data['phone'],
+                'email' => $data['email'],
+                'website' => $data['website'],
+                'description' => $data['description'],
+                'rating' => $data['rating'],
+                'image' => $data['image'],
+                'favicon' => $data['favicon'],
+                'logo' => $data['logo'],
+                'status' => $data['status'],
+                'is_active' => $data['is_active'],
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
-        }
 
+            // Create branch config for the restaurant
+            BranchConfig::create([
+                'branch_id' => $restaurant->id,
+                'tax' => $data['tax'] ?? 0, // Default tax to 0 if not provided
+                'currency' => $data['currency'] ?? 'USD', // Default currency to USD if not provided
+                'dial_code' => $data['dial_code'] ?? '+1', // Default dial code to +1 if not provided
+            ]);
+        }
 
         $this->command->info('Restaurants imported successfully from JSON file.');
     }
